b30dd71afee11d2fd6df0db737ec14c2fd4307f4
Aleksandra <aleksa831273@gmail.com>
2023-10-07 09:35:42 +0300

    Add NewNewTask1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,14 @@
 ﻿Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number < 100 || number >=1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста, повторите ввод");
+    return;
+}
+
+Console.WriteLine($"Введённое число -> {number}");
+
 int firstsecondDigit = number / 10;
 int secondDigit = firstsecondDigit % 10;
 
