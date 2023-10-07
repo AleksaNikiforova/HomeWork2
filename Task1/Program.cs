8ba29c958319c8a232007828665a264592c9e8e4
Aleksandra <aleksa831273@gmail.com>
2023-10-07 09:28:16 +0300

    Add NewTask1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,8 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+int firstsecondDigit = number / 10;
+int secondDigit = firstsecondDigit % 10;
+
+
+Console.WriteLine($"Вторая цифра -> {secondDigit}");
