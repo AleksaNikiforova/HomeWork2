ac1108840ff51fddd0f4103e935b74947e00043c
Aleksandra <aleksa831273@gmail.com>
2023-10-14 22:40:34 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿int RaiseToDegree (int num, int deg)
+{
+    int count = 0;
+    int multiple = 1 * num;
+    while (count < deg - 1)
+    {
+        multiple = multiple * num;
+        count ++;
+    }
+    return multiple;
+}    
+
+Console.WriteLine("Введите целое число, которое надо возвести в степень: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое положительное число, которое обозначает степень, в которую надо возвести предыдущее число: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int raiseToDegree = RaiseToDegree (number, degree);
+Console.WriteLine($"Число {number}, возведённое в степень {degree} равно {raiseToDegree}");
+
