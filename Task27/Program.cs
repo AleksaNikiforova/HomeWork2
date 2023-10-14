2547645fd558331288591f4101c1122e42f201a3
Aleksandra <aleksa831273@gmail.com>
2023-10-14 22:51:24 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿int SumDigitInNumber(int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigitInNumber = SumDigitInNumber(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {sumDigitInNumber}");
