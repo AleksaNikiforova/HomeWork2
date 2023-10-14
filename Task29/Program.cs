7e5d279e0ee997cc9f98df954d6c8ba80eb7364b
Aleksandra <aleksa831273@gmail.com>
2023-10-14 04:02:08 +0300

    add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] RandomArray (int min, int max, int length)
+{
+    int[] arr = new int [length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = RandomArray(1, 8, 8);
+PrintArray(array);
