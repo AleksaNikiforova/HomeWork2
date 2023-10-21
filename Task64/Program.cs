1d01a4b716a3253f35a18dd74efa3bfc8f04e34f
Aleksandra <aleksa831273@gmail.com>
2023-10-21 21:08:15 +0300

    add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,11 @@
+﻿void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(n);
