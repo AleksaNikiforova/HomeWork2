9c213b708c3a0dab8e05630742412237b229a9f3
Aleksandra <aleksa831273@gmail.com>
2023-10-21 23:24:00 +0300

    add Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,8 @@
+﻿int FunctionAkkerman(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else if (num2 == 0) return FunctionAkkerman(num1 - 1, 1);
+    else return FunctionAkkerman(num1 - 1, FunctionAkkerman(num1, num2 - 1));
+}
+
+Console.WriteLine(FunctionAkkerman(3, 11));
