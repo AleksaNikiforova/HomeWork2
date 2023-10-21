a5b0745875ceb6b21cfaf6b941cb6f7b1e1a76ae
Aleksandra <aleksa831273@gmail.com>
2023-10-21 22:01:12 +0300

    add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿int SumElemInInterval(int num1, int num2)
+{
+    if(num1 == num2)
+    {
+        return num1;
+    }
+    else if(num1 > num2)
+    {
+        if(num1 == num2) return 0;
+        else return num1 + SumElemInInterval(num1 - 1, num2);
+    }
+    else
+    {
+        if(num1 == num2) return 0;
+        else return num1 + SumElemInInterval(num1 + 1, num2);     
+    }
+} 
+
+Console.WriteLine("Введите первое натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumElemInInterval(m, n));
